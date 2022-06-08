e465abc4b6ff46732c6a117ff67fba247aa4f492
Carlos cruz valencia <a2carcruzinfo@gmail.com>
2022-06-08 19:25:08 +0200

    \update\

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route('/')
+def shoppage():
+    return render_template("layouts/shoppage.html")
+
 if __name__ == '__main__':
     app.run()
